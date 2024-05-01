87752ac5643249c57105efe769e233ac92b701a0
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2024-05-01 20:40:48 +0300

    added changes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const { sendEmailResponse, newMessageEmail } = require('./sendEmail')
 
 const express = require('express')
+const cors = require('cors')
 
 const app = express();
 const PORT = process.env.PORT || 8080
@@ -10,6 +11,7 @@ const emailCheck = require('email-check')
 let date
 let year
 
+app.use(cors())
 app.use(express.json())
 app.use(express.static('public'))
 app.use(express.static('public/styles'))
