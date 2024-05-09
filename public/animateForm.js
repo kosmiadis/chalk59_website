327bab8ae547855314369a01ebbfe57fe9486fb3
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2024-05-09 18:39:29 +0300

    added change in styling

--- public/animateForm.js
+++ public/animateForm.js
@@ -1,14 +1,22 @@
 const inputs = form.querySelectorAll('input')
 const labels = form.querySelectorAll('label')
 
+//label changes position when the input if focused.
 inputs.forEach(input => input.addEventListener('focus', e => {
-    labels.forEach(label => {
-        if (label.parentNode.querySelector('input').value === '') {
-            label.style.top = '6px'
-        }
-        else {
-            label.style.top = '-18px'
-        }
-    })
+    const curr_label = e.target.parentNode.querySelector('label')
+    if (e.target.value === '') {
+        curr_label.style.top = '6px'
+    }
+    else {
+        curr_label.style.top = '-18px'
+    }
     e.target.parentNode.querySelector('label').style.top = '-18px'
+}))
+
+//label goes to starting point if the input is not focused.
+inputs.forEach(input => input.addEventListener('blur', e => {
+    const curr_label = e.target.parentNode.querySelector('label')
+    if (e.target.value === '') {
+        curr_label.style.top = '6px'
+    }
 }))
