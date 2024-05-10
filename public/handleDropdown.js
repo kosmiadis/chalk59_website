b1687bfc24bede817fdcfe32759a7358b42fdb29
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2024-05-10 23:19:49 +0300

    added changes

---
+++ public/handleDropdown.js
@@ -0,0 +1,17 @@
+const dropdownButtons = document.querySelectorAll('.dropdown_btn')
+
+dropdownButtons.forEach(btn => {
+    btn.addEventListener('click', e => {
+        if (e.target.classList.contains('open')) {
+            e.target.parentNode.style.height = 'initial'
+            e.target.style.transform = 'rotate(45deg)'
+            e.target.classList.remove('open')
+        }
+        else {
+            e.target.parentNode.style.height = '80px'
+            e.target.style.transform = 'rotate(0deg)'
+            e.target.classList.add('open')
+        }
+        
+    })
+})
