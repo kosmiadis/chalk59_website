40488c216b736a97bdd1905ef074f06b7949e481
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2024-05-13 21:32:57 +0300

    added changes

--- public/hamburger.js
+++ public/hamburger.js
@@ -4,25 +4,20 @@ const menu = document.querySelector('.links')
 
 window.addEventListener('load', e => {
     if (window.innerWidth < 769) {
-        menu.style.display = 'none'
+        menu.style.display = 'flex'
     }
 })
 
 window.addEventListener('resize', e => {
-    if (window.innerWidth > 769) {
-        menu.style.display = 'flex'
-    }
-    else {
-        menu.style.display = 'none'
-    }
+    menu.style.right = '-200px'
 })
 
 hamburger.addEventListener('click', e => {
-    if (menu.style.display === 'none') {
-        menu.style.display = 'flex'
+    if (menu.style.right !== '0px') {
+        menu.style.right = '0px'
     }
 })
 
 close.addEventListener('click', e => {
-    menu.style.display = 'none'
+    menu.style.right = '-200px'
 })
