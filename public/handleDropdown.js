da1865b42571dc0c369ad20366f5b15fb4c9feda
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2024-05-14 15:23:20 +0300

    added changes

--- public/handleDropdown.js
+++ public/handleDropdown.js
@@ -3,7 +3,7 @@ const dropdownButtons = document.querySelectorAll('.dropdown_btn')
 dropdownButtons.forEach(btn => {
     btn.addEventListener('click', e => {
         if (e.target.classList.contains('open')) {
-            e.target.parentNode.style.height = '250px'
+            e.target.parentNode.style.height = 'min-content'
             e.target.style.transform = 'rotate(45deg)'
             e.target.classList.remove('open')
         }
