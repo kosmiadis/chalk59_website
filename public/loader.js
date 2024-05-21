87861bcd27bf8c06b956879aa8a4c45d42e5a278
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2024-05-21 16:16:53 +0300

    added changes

--- public/loader.js
+++ public/loader.js
@@ -4,8 +4,3 @@ window.addEventListener('load', e => {
     document.body.querySelector("*").style.display = 'none'
     on_load_loader.style.display = 'none'
 })
-
-window.addEventListener('DOMContentLoaded', e => {
-    document.body.querySelector("*").style.display = 'initial'
-    on_load_loader.style.display = 'none'
-})
