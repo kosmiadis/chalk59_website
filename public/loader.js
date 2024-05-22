0aca76a487e62715bc193069c8c37135467ecf7d
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2024-05-22 17:12:04 +0300

    added gitignore

--- public/loader.js
+++ public/loader.js
@@ -1,6 +1,6 @@
 const on_load_loader = document.querySelector('.onload_loader')
 
 window.addEventListener('load', e => {
-    document.body.querySelector("*").style.display = 'none'
+    document.body.querySelector('*').style.display = 'none'
     on_load_loader.style.display = 'none'
 })
