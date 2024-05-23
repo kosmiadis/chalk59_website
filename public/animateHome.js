1d0dd2ae7be0bf14f4cbcf8f4cfa73e3232811c7
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2024-05-23 21:56:16 +0300

    added changes

--- public/animateHome.js
+++ public/animateHome.js
@@ -1,8 +1,9 @@
 const hero_p = document.querySelector('.hero_welcome')
 const hero_img = document.querySelector('#hero_logo')
 const hero_btn = document.querySelector('.hero_btn')
-const summary = document.querySelector('.summary')    
+const summary = document.querySelector('.summary')  
+const textWrapper = document.querySelector('.text_wrapper')  
 
 hero_btn.addEventListener('click', e => {
-    summary.scrollIntoView({behavior: 'smooth'})
+    textWrapper.scrollIntoView({behavior: 'smooth'})
 })
