9be51105a97e0974eccf1660fbc2df0d438b2334
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2024-05-25 13:18:04 +0300

    added changes

--- public/handleDropdown.js
+++ public/handleDropdown.js
@@ -1,35 +1,38 @@
-const dropdownButtons = document.querySelectorAll('.dropdown_btn')
-const dropdownTriggers = document.querySelectorAll('.dropdown_trigger')
+const dropdownImgs = document.querySelectorAll('.dropdown_btn')
+const dropdownBtns = document.querySelectorAll('.dropdown')
+const dropdownTexts = document.querySelectorAll('.service_text')
 
-dropdownButtons.forEach(btn => {
+dropdownImgs.forEach(btn => {
     btn.addEventListener('click', e => {
         if (e.target.classList.contains('open')) {
-            e.target.parentNode.style.height = 'min-content'
+            e.target.parentNode.children[2].style.height = 'min-content'
             e.target.style.transform = 'rotate(45deg)'
             e.target.classList.remove('open')
         }
         else {
-            e.target.parentNode.style.height = '80px'
+            e.target.parentNode.querySelector('div.text').style.height = '0px'
             e.target.style.transform = 'rotate(0deg)'
             e.target.classList.add('open')
         }
     })
 })
 
-dropdownTriggers.forEach(d => {
-    
-    d.addEventListener('click', e => {
-        console.log('hh')
-        if (e.target.classList.contains('service')) {
+dropdownBtns.forEach(btn => {
+    btn.addEventListener('click', e => {
+        const text = e.target.parentNode.parentNode.querySelector('div.service_text'); // Adjust selector to fit your structure
+        if (text.style.height === '170px') {
+            text.style.height = ''; // or '0px' if you want to explicitly set it to zero
+        } else {
+            closeDropdowns(); // Close other dropdowns before opening the clicked one
+            text.style.height = '170px';
+        }
+    });
+});
 
-            if (e.target.classList.contains('close')) {
-                e.target.style.height = 'min-content';
-                e.target.classList.remove('close')
-            }
-            else {
-                e.target.style.height = '80px';
-                e.target.classList.add('close')
-            }
-        }  
-    })
-})
+function closeDropdown (dropdown) {
+    dropdown.style.height = '0'
+}
+
+function closeDropdowns () {
+    dropdownTexts.forEach(txt => txt.style.height = '0')
+}
